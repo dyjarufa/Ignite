e237091d5e78b6ee4b22d46c035262779f509415
Jady Rufino <jadyrufa@gmail.com>
2021-07-20 18:45:34 -0300

    Header component

--- chapter-02/dtmoney/src/styles/global.ts
+++ chapter-02/dtmoney/src/styles/global.ts
@@ -6,7 +6,7 @@ export const GlobalStyle = createGlobalStyle`
     --red: #E52E4D;
     --blue: #5429CC;
 
-    --blue-light: ##6933FF;
+    --blue-light: #6933FF;
 
     --text-title: #363F5F;
     --text-body: ##969CB3;
@@ -21,7 +21,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   html {
-    @media (max-width: 1080ox){
+    @media (max-width: 1080px){
       font-size: 93.75%
     }
 
