4f64dd4c4ebb7ff4d50a554008eacfb33f2e118e
Jady Rufino <jadyrufa@gmail.com>
2021-09-21 22:14:04 -0300

    chore: app structure

---
+++ chapter-03/ignews/src/pages/_app.tsx
@@ -0,0 +1,10 @@
+
+import { AppProps} from 'next/app'
+
+import '../styles/home.scss'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ chapter-03/ignews/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head  from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+          <title>Start | ig.news 🎥</title>
+      </Head>
+      <h1>Hello world red</h1>
+    </>
+  )
+}
