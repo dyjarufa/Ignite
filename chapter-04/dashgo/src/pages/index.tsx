50db6904fd526666a9d2bb77fda22850a24d910b
jady-santos <jady.santos@vodafone.com>
2022-03-17 15:40:58 +0000

    feat: set up project configuration

--- chapter-04/dashgo/src/pages/index.tsx
+++ chapter-04/dashgo/src/pages/index.tsx
@@ -1,5 +1,53 @@
+import { Button, Flex, Input, Stack, FormLabel, FormControl } from "@chakra-ui/react";
+
 export default function Home() {
   return (
-    <h1>DashGO</h1>
+    <Flex
+      w='100vw'
+      h='100vh'
+      align='center'
+      justify='center'
+    >
+      <Flex
+        as="form"
+        width='100%'
+        maxWidth={360}
+        bg='gray.800'
+        p="8"
+        borderRadius={8}
+        flexDirection='column'
+      >
+        <Stack spacing='4'>
+          <FormControl> {/* FormControl substitui a div para evitar o espaçamento criado pelo Stack entre a label e o input*/}
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              name="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              size='lg'
+              id='email'
+            />
+          </FormControl>
+
+
+          <FormControl>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              name="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              size='lg'
+              id='password'
+            />
+          </FormControl>
+
+        </Stack>
+        <Button type="submit" mt='6' colorScheme='pink' size='lg'>Enter</Button>
+      </Flex>
+    </Flex>
   )
 }
